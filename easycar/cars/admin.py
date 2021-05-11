cd75f9d98818e5e4f2d7041d7af74a0e9dfa0548
jxcorra <xord49@gmail.com>
2021-05-12 00:06:18 +0300

    Register admin models

--- easycar/cars/admin.py
+++ easycar/cars/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from cars.models import Make, Model
+
+
+@admin.register(Make)
+class CarMakeAdmin(admin.ModelAdmin):
+    fields = ('name',)
+
+
+@admin.register(Model)
+class CarModelAdmin(admin.ModelAdmin):
+    fields = ('name', 'make',)
+    list_filter = ('make__name',)
