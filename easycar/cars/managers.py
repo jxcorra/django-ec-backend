e114564d9d25b7d8b247ab21faa73f1fd3b8f11a
jxcorra <xord49@gmail.com>
2021-05-18 23:23:46 +0300

    Add car manager

--- easycar/cars/managers.py
+++ easycar/cars/managers.py
@@ -1,11 +1,14 @@
 from typing import Dict
 
 from django.db.models import Manager, QuerySet
+from django.utils import timezone
 
 
 class ModelsManager(Manager):
-    def order_by_query(self, query: str) -> QuerySet:
-        return
-
     def filter_dynamic(self, query: Dict) -> QuerySet:
         return self.filter(**query)
+
+
+class CarManager(Manager):
+    def fresh_cars(self):
+        return self.filter(year__gte=timezone.now().year - 10)
--- easycar/cars/models.py
+++ easycar/cars/models.py
@@ -1,8 +1,10 @@
 from django.conf import settings
+from django.contrib.auth.models import User
 from django.core.files.storage import default_storage
 from django.db import models
 
 # Create your models here.
+from cars.managers import CarManager
 from core.models import TimestampedModel, SafeDeleteModel
 
 
@@ -14,7 +16,7 @@ class Make(TimestampedModel, SafeDeleteModel, models.Model):
 
 
 class Model(TimestampedModel, SafeDeleteModel, models.Model):
-    name = models.CharField(max_length=64, help_text='Car model', unique=True)
+    name = models.CharField(max_length=128, help_text='Car model', unique=True)
     make = models.ForeignKey(
         to=Make, on_delete=models.CASCADE, related_name='models',
     )
@@ -32,6 +34,8 @@ class Car(TimestampedModel, SafeDeleteModel, models.Model):
     vin = models.CharField(max_length=64, help_text='Car vin number', null=True, blank=True)
     year = models.IntegerField(help_text='Car released year')
     image = models.FileField(storage=default_storage, max_length=2 * 1024, null=True, blank=True)
+
+    objects = CarManager()
 
     def __str__(self):
         return f'Car owner={self.owner.username} model={self.model.name} year={self.year}'
