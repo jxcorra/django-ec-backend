eb0403e027053f8ab1793a6a73c65f636e75f52d
jxcorra <xord49@gmail.com>
2021-06-13 23:30:02 +0300

    Set allowed hosts

--- easycar/easycar/settings.py
+++ easycar/easycar/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o+hgwvksc2e4pt+&7h^o-a4nxl*$(83afe6$d-q_ie(ym^7i%)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    '0.0.0.0',
+]
 
 
 # Application definition
