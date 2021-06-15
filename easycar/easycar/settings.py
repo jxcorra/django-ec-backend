2e8cd40be2d02b5d45795276b63b1fb356232164
jxcorra <xord49@gmail.com>
2021-06-16 01:52:30 +0300

    Deploy as backend stack

--- easycar/easycar/settings.py
+++ easycar/easycar/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-o+hgwvksc2e4pt+&7h^o-a4nxl*$(83afe6$d-q_ie(ym^7i%)
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    '127.0.0.1',
-    '0.0.0.0',
+    '*'
 ]
 
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,12 @@
 echo "Running migrations"
 ./manage.py migrate
 
+echo "Uploading test data"
+./manage.py uploadcarmodels --with-reset --max-workers=2
+
 echo "Starting gunicorn"
 gunicorn easycar.wsgi \
-        --bind 0.0.0.0:8000 \
+        --bind 0.0.0.0:5000 \
         --reload \
         --max-requests 100 \
         --threads 2 \
